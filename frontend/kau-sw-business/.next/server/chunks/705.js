950081fcdad9b8dacb76d6325187f938fc9e3b22
imnotmoon <imnotmoon@gmail.com>
2021-06-30 20:42:42 +0900

    빌드 파일 업로드

---
+++ frontend/kau-sw-business/.next/server/chunks/705.js
@@ -0,0 +1,79 @@
+exports.id = 705;
+exports.ids = [705];
+exports.modules = {
+
+/***/ 3430:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9297);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9914);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+ // interface
+
+const Subtitle = ({
+  text
+}) => {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(Container, {
+    children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Bullet, {}), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+      children: text
+    })]
+  });
+};
+
+const Container = styled_components__WEBPACK_IMPORTED_MODULE_2___default().div.withConfig({
+  displayName: "Subtitle__Container",
+  componentId: "ln7m5c-0"
+})(["display:flex;align-items:top;margin-bottom:15px;& > div:nth-of-type(2){font-weight:500;color:#262f41;font-size:20px;}"]);
+const Bullet = styled_components__WEBPACK_IMPORTED_MODULE_2___default().div.withConfig({
+  displayName: "Subtitle__Bullet",
+  componentId: "ln7m5c-1"
+})(["width:10px;height:20px;background:#262f41;margin-left:5px;margin-right:10px;margin-top:6px;"]);
+/* harmony default export */ __webpack_exports__["Z"] = (Subtitle);
+
+/***/ }),
+
+/***/ 8237:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9297);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9914);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+ // interface
+
+const Title = ({
+  text
+}) => {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(Container, {
+    children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+      src: "/img/title_bullet.png",
+      alt: ""
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+      children: text
+    }), "\x1C"]
+  });
+};
+
+const Container = styled_components__WEBPACK_IMPORTED_MODULE_2___default().div.withConfig({
+  displayName: "Title__Container",
+  componentId: "sc-16q65c5-0"
+})(["display:flex;align-items:center;justify-content:flex-start;margin-bottom:10px;& > img{width:17px;height:17px;padding-right:10px;}& > span{font-size:22px;font-weight:600;color:#333;}"]);
+/* harmony default export */ __webpack_exports__["Z"] = (Title);
+
+/***/ })
+
+};
+;
