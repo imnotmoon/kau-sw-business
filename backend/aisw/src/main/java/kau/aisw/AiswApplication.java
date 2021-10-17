ecd31c46a4eeb98862355d061125e256ad88b037
ahrimy <dlsdjdkfla@gmail.com>
2021-10-17 18:30:04 +0900

    Init : 프로젝트 생성

---
+++ backend/aisw/src/main/java/kau/aisw/AiswApplication.java
@@ -0,0 +1,13 @@
+package kau.aisw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiswApplication.class, args);
+	}
+
+}
---
+++ backend/aisw/src/test/java/kau/aisw/AiswApplicationTests.java
@@ -0,0 +1,13 @@
+package kau.aisw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
