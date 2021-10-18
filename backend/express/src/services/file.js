6e55b1db2224321c406e99615f34537946929152
ahrimy <dlsdjdkfla@gmail.com>
2021-10-18 20:21:00 +0900

    Fix : File Service 로직 변경
    - 파일 업로드, 데이터 추가 한번에 처리하도록 수정
    - 파일 삭제, 데이터 삭제 한번에 처리하도록 수정

--- backend/express/src/services/file.js
+++ backend/express/src/services/file.js
@@ -1,21 +1,35 @@
 const fse = require('fs-extra');
+const { fileNameGenerator } = require('../utils/filename');
 const db = require('../db/models');
 const { File } = db;
 
 const FileService = {
   /**
    * Single 파일 데이터 추가
-   * @param {Object} data filename, name, noticeId
+   * @param {String} noticeId
+   * @param {Object} file
    * @returns
    */
-  add: async (data) => File.create(data),
+  add: async (noticeId, file) => {
+    file.filename = fileNameGenerator(file.originalname);
+    uploadFile(file);
+    return File.create({ filename: file.filename, name: file.originalname, noticeId });
+  },
 
   /**
    * Mupltiple 파일 데이터 추가
-   * @param {Object} data [{filename, name, noticeId},,,]
+   * @param {String} noticeId
+   * @param {Array} files [file1, file2,,,]
    * @returns
    */
-  addAll: async (data) => File.bulkCreate(data),
+  addAll: async (noticeId, files) => {
+    const uploadedFiles = files.map((file) => {
+      file.filename = fileNameGenerator(file.originalname);
+      uploadFile(file);
+      return { filename: file.filename, name: file.originalname, noticeId };
+    });
+    return File.bulkCreate(uploadedFiles);
+  },
 
   /**
    * 파일 데이터 조회
@@ -25,25 +39,41 @@ const FileService = {
   findOne: async (id) => File.findByPk(id),
 
   /**
-   * 파일 데이터 삭제
+   * Single 파일 삭제
    * @param {Number} id
    * @returns
    */
-  delete: async (id) => File.destroy({ where: { id } }),
+  delete: async (id) => {
+    const file = await File.findByPk(id, { attributes: ['filename'] });
+    if (!file) return;
+    removeFile(file.filename);
+    return File.destroy({ where: { id } });
+  },
 
   /**
-   * 파일 저장
-   * @param {Array} files
+   * Multiple 파일 삭제
+   * @param {Array} ids [id1, id2,,,]
    * @returns
    */
-  uploadFile: async (file) => fse.outputFile(`${process.env.PWD}/files/${file.filename}`, file.buffer),
-
-  /**
-   * 파일 삭제
-   * @param {String} filename
-   * @returns
-   */
-  removeFile: async (filename) => fse.remove(`${process.env.PWD}/files/${filename}`),
+  deleteAll: async (ids) => {
+    const files = await File.findAll({ where: { id: ids }, attributes: ['filename'] });
+    files.forEach(({ filename }) => removeFile(filename));
+    return File.destroy({ where: { id: ids } });
+  },
 };
+
+/**
+ * 파일 저장
+ * @param {Array} files
+ * @returns
+ */
+const uploadFile = async (file) => fse.outputFile(`${process.env.PWD}/files/${file.filename}`, file.buffer);
+
+/**
+ * 파일 삭제
+ * @param {String} filename
+ * @returns
+ */
+const removeFile = async (filename) => fse.remove(`${process.env.PWD}/files/${filename}`);
 
 module.exports = FileService;
