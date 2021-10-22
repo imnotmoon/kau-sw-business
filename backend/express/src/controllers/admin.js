85734d3d3d2a40617f7eb9fd6261ed055ce32258
ahrimy <dlsdjdkfla@gmail.com>
2021-10-22 18:12:52 +0900

    Feat : Admin Controller 구현
    - Admin 전체 조회
    - 특정 Admin 조회
    - Admin 추가
    - Admin 수정
    - Admin 삭제

---
+++ backend/express/src/controllers/admin.js
@@ -0,0 +1,53 @@
+const createError = require('http-errors');
+// const bcrypt = require('bcryptjs');
+const AdminService = require('../services/admin');
+
+const AdminController = {
+  getAdminByPk: async (req, res, next) => {
+    const { id } = req.params;
+
+    const admin = await AdminService.findOne(id);
+    if (!admin) return next(createError(404, '존재하지 않는 ID'));
+
+    return res.status(200).json({ data: admin });
+  },
+
+  getAdmins: async (req, res) => {
+    const admins = await AdminService.findAll();
+    return res.status(200).json({ data: admins });
+  },
+
+  createAdmin: async (req, res, next) => {
+    const { userId, password, name } = req.body;
+
+    const result = await AdminService.add({
+      userId,
+      password /*password: bcrypt.hashSync(password, 8)*/,
+      name,
+    });
+
+    if (!result || !result.id) return next(createError(500));
+
+    return res.status(201).json({ success: true, adminId: result.id });
+  },
+
+  updateAdmin: async (req, res, next) => {
+    const { id, ...rest } = req.body;
+
+    if (!id) return next(createError(400, 'id is required'));
+    await AdminService.update(id, rest);
+
+    return res.status(200).json({ success: true });
+  },
+
+  removeAdmin: async (req, res, next) => {
+    const { id } = req.params;
+
+    const deleted = await AdminService.delete(id);
+    if (!deleted) return next(createError(404, '존재하지 않는 ID'));
+
+    return res.status(200).json({ success: true });
+  },
+};
+
+module.exports = AdminController;
