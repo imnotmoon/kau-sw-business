5f3c9bd14b122eae61fe1687ea0bd4ef6206c7f1
ahrimy <dlsdjdkfla@gmail.com>
2021-11-07 23:05:08 +0900

    Fix : warning 주석처리

--- admin/src/pages/MainPage.tsx
+++ admin/src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import styled from "@emotion/styled";
+// import styled from "@emotion/styled";
 
 import Layout from "../components/Layout";
 
