404f32957d6f989e44e67aa7d20e4996c42a19f0
ahrimy <dlsdjdkfla@gmail.com>
2021-11-28 22:40:37 +0900

    chore: dev 모드에서도 sqlite 대신 mysql 연결하도록 변경

--- backend/express/src/db/config/config.js
+++ backend/express/src/db/config/config.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    storage: `${process.env.PWD}/sqlite/database.sqlite`,
-    dialect: 'sqlite',
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    timezone: '+09:00',
+    dialect: 'mysql',
+    loggin: false,
   },
   production: {
     username: process.env.MYSQL_USER,
