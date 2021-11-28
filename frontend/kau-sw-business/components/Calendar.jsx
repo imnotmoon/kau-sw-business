64d7b16d91ca060563c01964ea1f14fca3b8af4d
ahrimy <dlsdjdkfla@gmail.com>
2021-11-29 00:23:09 +0900

    feat: 캘린더 페이지 월 단위로 렌더링

--- frontend/kau-sw-business/components/Calendar.jsx
+++ frontend/kau-sw-business/components/Calendar.jsx
@@ -1,20 +1,80 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import moment from "moment";
 
-const Calendar = ({ rowNames, data }) => {
+const Calendar = ({ data, scheduleTable, maxIndex }) => {
   return (
-    <Table>
-      <th>
-        {MONTHS.map((month) => (
-          <td>{month}</td>
-        ))}
-      </th>
-    </Table>
+    <Wrapper>
+      <Table>
+        <thead>
+          <tr>
+            {MONTHS.map((month) => (
+              <TableHeadColumn key={month}>{month}</TableHeadColumn>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {new Array(maxIndex + 1).fill(0).map((_, index) => {
+            const doneList = [];
+            return (
+              <tr key={index}>
+                {scheduleTable[index].map((schedule, idx) => {
+                  if (!schedule) return <TableData key={idx} />;
+                  if (doneList.includes(schedule.id)) return;
+
+                  const colSpan =
+                    moment(schedule.endDate).month() -
+                    moment(schedule.startDate).month() +
+                    1;
+                  doneList.push(schedule.id);
+                  return (
+                    <TableData key={schedule.id} colSpan={colSpan}>
+                      {schedule.title}
+                    </TableData>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </Wrapper>
   );
 };
 
-const Table = styled.table``;
+const Wrapper = styled.div`
+  overflow-x: scroll;
+`;
 
-const MONTHS = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
+const Table = styled.table`
+  border-collapse: collapse;
+`;
+
+const TableHeadColumn = styled.th`
+  min-width: 100px;
+  border: 1px solid;
+`;
+
+const TableData = styled.td`
+  border: 1px solid;
+  text-align: center;
+  word-break: keep-all;
+`;
+
+const MONTHS = [
+  "1월",
+  "2월",
+  "3월",
+  "4월",
+  "5월",
+  "6월",
+  "7월",
+  "8월",
+  "9월",
+  "10월",
+  "11월",
+  "12월",
+];
 
 export default Calendar;
--- frontend/kau-sw-business/pages/major_education/calendar.jsx
+++ frontend/kau-sw-business/pages/major_education/calendar.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
-import ContentFrame from '../../components/ContentFrame';
-import withHead from '../../components/hoc/withHead';
+import React from "react";
+import axios from "axios";
+import moment from "moment";
 
-const Calendar = () => {
-  return <ContentFrame title="캘린더" state={1}></ContentFrame>;
+import ContentFrame from "../../components/ContentFrame";
+import withHead from "../../components/hoc/withHead";
+import CalendarLayout from "../../components/Calendar";
+
+const Calendar = (props) => {
+  return (
+    <ContentFrame title="캘린더" state={1}>
+      <CalendarLayout {...props} />
+    </ContentFrame>
+  );
 };
 
-export default withHead(Calendar, '');
+export const getServerSideProps = async () => {
+  // 현재 연도 일정 조회
+  const today = moment();
+  const startDate = today.startOf("year").format("YYYY-MM-DD");
+  const endDate = today.endOf("year").format("YYYY-MM-DD");
+  const { data } = await axios.get(
+    `${process.env.API_BASE_URL}/schedule?category=major&from=${startDate}&to=${endDate}`
+  );
+  // 캘린더 테이블에 들어가는 순서 정하기 위해 하나씩 확인
+  const scheduleTable = {};
+  let maxIndex = 0;
+  data.data.forEach((row) => {
+    // 스케줄 시작 월, 종료 월
+    const from = moment(row.startDate).startOf("month");
+    const to = moment(row.endDate).endOf("month");
+    let index = 0; // 캘린더에서 row 순서
+    while (true) {
+      if (!scheduleTable[index]) scheduleTable[index] = new Array(12).fill(null);
+      const isAvailable = new Array(to.month() - from.month() + 1)
+        .fill(0)
+        .every((_, idx) => {
+          return !scheduleTable[index][from.month() + idx]}
+          );
+      if (isAvailable) {
+        new Array(to.month() - from.month() + 1)
+        .fill(0)
+        .forEach((_, idx) => scheduleTable[index][from.month() + idx] = row);
+        maxIndex = Math.max(maxIndex, index);
+        break;
+      }
+      index++;
+    }
+  });
+  return { props: { ...data, scheduleTable, maxIndex } };
+};
+
+export default withHead(Calendar);
