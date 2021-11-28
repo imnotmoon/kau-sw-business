0c1e1ba7c8dbed3d72dd4951155a734d41f7f619
ahrimy <dlsdjdkfla@gmail.com>
2021-11-29 00:33:48 +0900

    fix: 중복 id 확인 array 대신 set 사용하게 변경

--- frontend/kau-sw-business/pages/influences/calendar.jsx
+++ frontend/kau-sw-business/pages/influences/calendar.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ContentFrame from '../../components/ContentFrame';
-import withHead from '../../components/hoc/withHead';
+import React from "react";
+import moment from "moment";
+import ContentFrame from "../../components/ContentFrame";
+import withHead from "../../components/hoc/withHead";
 
 const Calendar = () => {
   return <ContentFrame title="캘린더" state={4}></ContentFrame>;
+};
+
+export const getServerSideProps = async () => {
+  const today = moment();
+  const startDate = today.startOf("year").format("YYYY-MM-DD");
+  const endDate = today.endOf("year").format("YYYY-MM-DD");
+  const { data } = await axios.get(
+    `${process.env.API_BASE_URL}/schedule?category=influences&from=${startDate}&to=${endDate}`
+  );
+  return { props: data };
 };
 
 export default withHead(Calendar);
