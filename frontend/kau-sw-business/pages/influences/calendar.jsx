a606a2351ad9ea7035877db15fabc048260342fd
ahrimy <dlsdjdkfla@gmail.com>
2021-11-29 21:20:42 +0900

    fix: axios import 안해서 생긴 오류 해결

--- frontend/kau-sw-business/pages/influences/calendar.jsx
+++ frontend/kau-sw-business/pages/influences/calendar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import moment from "moment";
 import ContentFrame from "../../components/ContentFrame";
 import withHead from "../../components/hoc/withHead";
