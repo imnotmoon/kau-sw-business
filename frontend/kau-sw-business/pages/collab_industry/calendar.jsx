e3c128f8df381a7478895da2672c171be2221be9
ahrimy <dlsdjdkfla@gmail.com>
2021-12-05 03:53:45 +0900

    feat: 각 페이지 캘린더에 일정 리스트 추가

--- frontend/kau-sw-business/pages/collab_industry/calendar.jsx
+++ frontend/kau-sw-business/pages/collab_industry/calendar.jsx
@@ -6,11 +6,13 @@ import { generateScheduleTable } from "../../utils/ScheduleTableGenerator";
 import ContentFrame from "../../components/ContentFrame";
 import withHead from "../../components/hoc/withHead";
 import CalendarLayout from "../../components/Calendar";
+import ScheduleLayout from "../../components/Schedule";
 
 const Calendar = (props) => {
   return (
     <ContentFrame title="캘린더" state={2}>
       <CalendarLayout {...props} />
+      <ScheduleLayout {...props} />
     </ContentFrame>
   );
 };
@@ -20,12 +22,28 @@ export const getServerSideProps = async () => {
   const today = moment();
   const startDate = today.startOf("year").format("YYYY-MM-DD");
   const endDate = today.endOf("year").format("YYYY-MM-DD");
-  const { data } = await axios.get(
-    `${process.env.API_BASE_URL}/schedule?category=collab&from=${startDate}&to=${endDate}&type=duration`
-  );
-  const { scheduleTable, maxIndex } = generateScheduleTable(data.data);
 
-  return { props: { ...data, scheduleTable, maxIndex } };
+  const getDataByDuration = axios
+    .get(
+      `${process.env.API_BASE_URL}/schedule?category=collab&from=${startDate}&to=${endDate}&type=duration`
+    )
+    .then(({ data }) => data)
+    .catch((err) => console.log(err));
+  const getDataByDate = axios
+    .get(
+      `${process.env.API_BASE_URL}/schedule?category=collab&from=${startDate}&to=${endDate}`
+    )
+    .then(({ data }) => data)
+    .catch((err) => console.log(err));
+  const [dataByDuration, dataByDate] = await Promise.all([
+    getDataByDuration,
+    getDataByDate,
+  ]);
+
+  const { scheduleTable, maxIndex } = generateScheduleTable(
+    dataByDuration.data
+  );
+  return { props: { scheduleTable, maxIndex, scheduleList: dataByDate.data } };
 };
 
 export default withHead(Calendar);
