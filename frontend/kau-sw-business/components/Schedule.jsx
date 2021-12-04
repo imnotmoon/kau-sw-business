12be99f4eeb7f9435b6513254168d8e7b5ff74ca
ahrimy <dlsdjdkfla@gmail.com>
2021-12-05 03:50:37 +0900

    feat: 일정 목록 테이블 추가

---
+++ frontend/kau-sw-business/components/Schedule.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import moment from "moment";
+import { COLORS } from "../styles/theme";
+
+const Schedule = ({ scheduleList, scheduleTable, maxIndex }) => {
+  moment.locale("ko", {
+    weekdays: [
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+    ],
+    weekdaysShort: ["일", "월", "화", "수", "목", "금", "토"],
+  });
+  return (
+    <Wrapper>
+      <Table>
+        <tbody>
+          {scheduleList.map((schedule) => (
+            <TableRow key={schedule.id}>
+              <TableData width="30%">
+                {moment(schedule.startDate).format("YYYY.MM.DD(ddd)")}
+                {(schedule.startDate !== schedule.endDate) && 
+                ` ~ ${moment(schedule.endDate).format("YYYY.MM.DD(ddd)")}`}
+              </TableData>
+              <TableData width="70%">{schedule.title}</TableData>
+            </TableRow>
+          ))}
+        </tbody>
+      </Table>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  color: ${COLORS.PRIMARY1}
+`;
+
+const Table = styled.table`
+  margin: auto;
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const TableRow = styled.tr`
+  height: 55px;
+
+  > td: first-child {
+      background-color: ${COLORS.BACKGROUND}
+  }
+`;
+
+const TableData = styled.td`
+  text-align: left;
+  width: ${({ width = "200px" }) => width};
+  padding-left: 10px;
+  border-top: 1px solid ${COLORS.PRIMARY1};
+  border-bottom: 1px solid ${COLORS.PRIMARY1};
+  word-break: keep-all;
+`;
+
+export default Schedule;
