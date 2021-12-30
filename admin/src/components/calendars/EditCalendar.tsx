06373d8a44c1e7fd5c555786306d6117d11c1641
imnotmoon <imnotmoon@gmail.com>
2021-12-30 13:50:36 +0900

    feat: admin 일정 메뉴 타이틀 설정

--- admin/src/components/calendars/EditCalendar.tsx
+++ admin/src/components/calendars/EditCalendar.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import { Title } from '../notices/NewNoticeStyle';
 
 const EditCalendar = () => {
   return (
     <Container>
+      <Title>일정 수정/삭제</Title>
       
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.div`
+	width: 100%;
+	background-color: rgba(255, 255, 255, 0.2);
+	backdrop-filter: blur(100px);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 
 export default EditCalendar
--- admin/src/components/calendars/NewCalendar.tsx
+++ admin/src/components/calendars/NewCalendar.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import { Title } from '../notices/NewNoticeStyle'
 
 const NewCalendar = () => {
   return (
     <Container>
-      
+      <Title>일정 등록</Title>
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.div`
+	width: 100%;
+	background-color: rgba(255, 255, 255, 0.2);
+	backdrop-filter: blur(100px);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 
 export default NewCalendar
